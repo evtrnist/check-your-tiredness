cc0625e3cf4d52cf0f944cd4b623e6e0ebca91c5
evtrnist <evagrandet@ro.ru>
2022-05-27 19:38:51 +0300

    update method

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { Hears, Help, On, Start, Update } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
+import { UserService } from './database/entities/user/user.service';
 
 @Update()
 @Injectable()
 export class AppService {
+  constructor(private readonly userService: UserService) {}
   getData(): { message: string } {
     return { message: 'Welcome to server!' };
   }
 
   @Start()
   async startCommand(ctx: Context) {
-    console.log(ctx);
-    await ctx.reply('Welcomeee');
+    const hasUser = (await this.userService.getUsers()).find(
+      (user) => user.id === ctx.from.id,
+    );
+
+    if (hasUser) {
+      await ctx.reply(`Я тебя знаю, ${ctx.from.id}`);
+    } else {
+      await ctx.reply(`Ты кто, ${ctx.from.id}`);
+    }
   }
 
   @Help()
